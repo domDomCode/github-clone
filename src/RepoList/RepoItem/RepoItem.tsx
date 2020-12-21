02649177152dd40797e17dfff6fc3c1eee02318d
Dominik Stasiak <domin.stasiak@gmail.com>
2020-12-21 13:06:36 +0100

    Wrap up on styles, switched to Primer icons, fixed mutation logic

--- src/RepoList/RepoItem/RepoItem.tsx
+++ src/RepoList/RepoItem/RepoItem.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 import { RepoInterface } from '../RepoList';
-import { Box, Button as PrimerButton, Link, Text } from '@primer/components';
+import { Box, Button as PrimerButton, Grid, Link, Text } from '@primer/components';
 import { gql, useMutation } from '@apollo/client';
-import { ForkIcon, LicenseIcon, StarIcon } from '../../icons/icons';
-import { border } from 'styled-system'
+import { border } from 'styled-system';
+import { differenceInCalendarDays, format, parseISO } from 'date-fns';
+import { LawIcon, RepoForkedIcon, StarFillIcon, StarIcon } from '@primer/octicons-react';
 
 const ADD_STAR = gql`
 mutation AddStar($repoId: ID!) {
@@ -13,7 +14,7 @@ mutation AddStar($repoId: ID!) {
     }
   }
 }
-`
+`;
 
 const REMOVE_STAR = gql`
 mutation RemoveStar($repoId: ID!) {
@@ -23,7 +24,7 @@ mutation RemoveStar($repoId: ID!) {
     }
   }
 }
-`
+`;
 
 interface AddStarResponseInterface {
   addStar: {
@@ -45,27 +46,63 @@ interface Props {
   repo: RepoInterface
 }
 
-const RepoItem: FC<Props> = ({repo}) => {
-  const [addStar, {data: addStarData} ] = useMutation<AddStarResponseInterface>(ADD_STAR);
-  const [removeStar, {data: removeStarData} ] = useMutation<RemoveStarResponseInterface>(REMOVE_STAR);
+const RepoItem: FC<Props> = ({ repo }) => {
+  const [ addStar, { data: addStarData } ] = useMutation<AddStarResponseInterface>(ADD_STAR);
+  const [ removeStar, { data: removeStarData } ] = useMutation<RemoveStarResponseInterface>(REMOVE_STAR);
   // TODO implement removeStar!!!
   // TODO change star based on return response as well? dedicated const isStarred, that depends on both
 
   // if mutated, switch to displaying value from the mutation response
-  const isStarred = addStarData?.addStar.starrable.viewerHasStarred || repo.node.viewerHasStarred;
+  const [ isStarred, setIsStarred ] = useState<boolean | null>(repo.node.viewerHasStarred);
+
+  useEffect(() => {
+    (addStarData || removeStarData) && setIsStarred(prevState => !prevState);
+  }, [ addStarData, removeStarData ]);
+
+  const handleAddRemoveStar = (): void => {
+    const mutationOptions = { variables: { repoId: repo.node.id } };
+
+
+    isStarred ? removeStar(mutationOptions) : addStar(mutationOptions);
+  };
+
+  const getUpdatedAtInfo = (): ReactNode => {
+    const updatedAtDate = parseISO(repo.node.updatedAt);
+
+    const differenceInDays = differenceInCalendarDays(
+      new Date(),
+      updatedAtDate
+    );
+
+    if (differenceInDays === 0) {
+      return null;
+    } else if (differenceInDays <= 10) {
+      return <Text color={'gray.6'} fontSize={12}>Updated {differenceInDays} ago</Text>;
+    } else {
+      return <Text color={'gray.6'} fontSize={12}>Updated on {format(updatedAtDate, 'MMM dd')}</Text>;
+    }
+  };
+
+  const repoLink = `${repo.node.owner.url}/${repo.node.name}`;
 
   return (
     <Box
       py={4}
       display={'flex'}
       flexDirection={'row'}
-      style={{borderBottom: '1px solid #d1d5da'}}
+      style={{ borderBottom: '1px solid #d1d5da' }}
       justifyContent={'space-between'}
       alignItems={'center'}
     >
       <Box display={'flex'} flexDirection={'column'} alignItems={'flex-start'}>
         <Box>
-          <Link as={'h3'} my={2} href={`${repo.node.owner.url}/${repo.node.name}`} >{repo.node.name}</Link>
+          <Link as={'h3'}
+                my={2}
+                style={{ cursor: 'pointer' }}
+                href={repoLink}
+          >
+            {repo.node.name}
+          </Link>
         </Box>
         <Box display={'inline-flex'} justifyContent={'flex-start'} mb={2} pr={4}>
           <Text
@@ -76,43 +113,58 @@ const RepoItem: FC<Props> = ({repo}) => {
             {repo.node.description}
           </Text>
         </Box>
-        <Box display={'flex'} mt={2}>
+        <Box display={'flex'} alignItems={'center'} mt={2}>
           {repo.node.primaryLanguage ? (
             <Text mr={3} color={'gray.6'} fontSize={12}>
               {repo.node?.primaryLanguage.name}
             </Text>
           ) : null}
-          <Box mr={3} display={'flex'} alignItems={'center'}>
-            {StarIcon}
-            <Text color={'gray.6'} pl={1} fontSize={12}>{repo.node.stargazerCount}</Text>
-          </Box>
-          <Box mr={3} display={'flex'} alignItems={'center'}>
-            {ForkIcon}
-            <Text color={'gray.6'} pl={1} fontSize={12}> {repo.node.forkCount}</Text>
-          </Box>
+          <Link
+            href={`${repoLink}/stargazers`}
+            mr={3}
+            color={'gray.6'}
+            muted={true}
+            style={{ cursor: 'pointer' }}
+            fontSize={12}
+          >
+            <StarIcon size={16}/>
+            <Text pl={1}>{repo.node.stargazerCount}</Text>
+          </Link>
+          <Link
+            href={`${repoLink}/network/members`}
+            mr={3}
+            color={'gray.6'}
+            muted={true}
+            style={{ cursor: 'pointer' }}
+            fontSize={12}
+          >
+            <RepoForkedIcon size={16}/>
+            <Text pl={1}>{repo.node.forkCount}</Text>
+          </Link>
           {repo.node.licenseInfo ? (
-            <Box mr={3} display={'flex'} alignItems={'center'}>
-              {LicenseIcon}
+            <Box mr={3} display={'flex'} alignItems={'center'} color={'gray.6'}>
+              <LawIcon size={16}/>
               <Text color={'gray.6'} pl={1} fontSize={12}>
                 {repo.node?.licenseInfo.name}
               </Text>
             </Box>
           ) : null}
-          <Text color={'gray.6'} fontSize={12}>{repo.node.updatedAt}</Text>
+          <Box>{getUpdatedAtInfo()}</Box>
         </Box>
       </Box>
-      <Box width={100}>
+      <Box>
         <PrimerButton
           display={'flex'}
-          style={{alignItems: 'center'}}
+          style={{ alignItems: 'center' }}
           color={'gray.6'}
-          onClick={() => addStar({variables: {repoId: repo.node.id}})}
+          onClick={() => handleAddRemoveStar()}
         >
-          {StarIcon} Star {isStarred ? 'yes' : 'no'}
+          {isStarred ? <StarFillIcon size={16}/> : <StarIcon size={16}/>}
+          <Text color={'gray.6'} pl={1}>Star</Text>
         </PrimerButton>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default RepoItem;
